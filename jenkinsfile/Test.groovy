4826a1ce21bd11f783f72fc050d57619aaf88fba
suda23 <akicya@foxmail.com>
2021-12-15 17:33:22 +0800

    fix

--- jenkinsfile/Test.groovy
+++ jenkinsfile/Test.groovy
@@ -8,8 +8,8 @@ def remote = new org.devops.remote()
 pipeline {
     agent any
     parameters {
-        // string(name:"ip_address",description: '目标ip地址',choices: ['10.6.213.10', '10.6.213.20', '10.6.213.30','10.6.213.40','10.6.213.50'])
-        string(name: "ip_address", description: '目标ip地址', defaultValue: '')
+         choice(name:"ip_address",description: '目标ip地址',choices: ['10.6.213.10', '10.6.213.20', '10.6.213.30','10.6.213.40','10.6.213.50'])
+//        string(name: "ip_address", description: '目标ip地址', defaultValue: '')
         choice(name: "version", description: '安装版本号', choices: ['dce-4.0.7-35110', 'dce-4.0.7-35143'])
     }
     stages {
